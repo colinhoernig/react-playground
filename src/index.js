bc7366848236e047687f1a8a835cb121ec588caf
Colin Hoernig <Hoernig.Colin@gmail.com>
2016-02-09 21:02:45 -0600

    Add API key handling and YouTube API dependency.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Google YouTube API Key
+import { API_KEY } from '../API_KEY';
+
 // Create a simple component
 const App = () => {
   return <div>Hi</div>;
