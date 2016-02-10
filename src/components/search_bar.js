be67d960f49d2349e29515e5435e7511b4d60e22
Colin Hoernig <Hoernig.Colin@gmail.com>
2016-02-10 13:43:13 -0600

    Bring the YTSearch functionality into own method, wire it up to search bar, and debounce it.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,15 +4,12 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    // Initialize state
     this.state = { term: '' };
-
-    // Bind event handlers for proper `this` context
-    this.onInputChange = this.onInputChange.bind(this);
   }
 
-  onInputChange(event) {
-    this.setState({ term: event.target.value });
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
   }
 
   render() {
@@ -20,7 +17,7 @@ class SearchBar extends Component {
       <div className="search-bar">
         <input
           value={this.state.term}
-          onChange={this.onInputChange}
+          onChange={event => this.onInputChange(event.target.value)}
           className="form-control" />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 // Components
 import SearchBar from './components/search_bar';
@@ -19,7 +20,11 @@ class App extends Component {
       selectedVideo: null,
     };
 
-    YTSearch({ key: API_KEY, term: 'Snowboarding' }, videos => {
+    this.videoSearch('snowboarding');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -28,9 +33,12 @@ class App extends Component {
   }
 
   render() {
+    // Debounce the search
+    const videoSearch = _.debounce(term => { this.videoSearch(term); }, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
