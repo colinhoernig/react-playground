f7214c3bf8fa6660f9962c8baa4d20b3517e3788
Colin Hoernig <Hoernig.Colin@gmail.com>
2016-02-11 22:55:25 -0600

    Import OpenWeatherMaps API key in actions, not src index.

--- weatheredux/src/index.js
+++ weatheredux/src/index.js
@@ -6,9 +6,6 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
-// Google YouTube API Key
-import { API_KEY } from '../API_KEY';
-
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 // Mount app to DOM
