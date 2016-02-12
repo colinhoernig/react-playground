4a7c0ab82ce2f55ac710f09e998058990d639542
Colin Hoernig <Hoernig.Colin@gmail.com>
2016-02-11 23:32:07 -0600

    Add redux-promise middleware.  Initial wire up of SearchBar to make ajax request to OpenWeatherMaps API (using axios lib).

--- weatheredux/src/actions/index.js
+++ weatheredux/src/actions/index.js
@@ -1,1 +1,16 @@
-import API_KEY from '../../API_KEY';
+import axios from 'axios';
+
+import { API_KEY } from '../../API_KEY';
+const API_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const searchUrl = `${API_URL}&q=${city},us`; // Only cities in United States for now
+  const request = axios.get(searchUrl);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
--- weatheredux/src/containers/search-bar.js
+++ weatheredux/src/containers/search-bar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions';
 
 class SearchBar extends Component {
   constructor(props) {
@@ -7,6 +10,7 @@ class SearchBar extends Component {
     this.state = { term: ''};
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -15,6 +19,11 @@ class SearchBar extends Component {
 
   onFormSubmit(event) {
     event.preventDefault();
+
+    // fetchWeather action dispatch available on this.props
+    // because of mapDispatchToProps and connect
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' }); // clear the input
   }
 
   render() {
@@ -33,4 +42,9 @@ class SearchBar extends Component {
   }
 }
 
-export default SearchBar;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// connect first argument null because no state to map to props
+export default connect(null, mapDispatchToProps)(SearchBar);
